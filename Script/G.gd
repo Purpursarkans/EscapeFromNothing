c5378c826e364ff282683fe4f754821f65b10b8a
Purpursarkans <goga.danilov10@gmail.com>
2024-02-02 16:28:04 +0400

    Sensivity to global

--- Script/G.gd
+++ Script/G.gd
@@ -2,10 +2,13 @@ extends Node
 
 var object = null
 
+var Sensivity : float = 10
+var FixSensivity : float = 0.0001
 
 func _ready():
 	#get_window().size = Vector2(320, 240)
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	pass
 func _process(delta):
 	pass
 
