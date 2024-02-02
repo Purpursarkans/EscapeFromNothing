6b9b741cb3ec97ac9c7d0ea77876de6d2f0f9a7d
Purpursarkans <goga.danilov10@gmail.com>
2024-02-02 13:37:12 +0400

    test upd

--- Script/G.gd
+++ Script/G.gd
@@ -3,6 +3,9 @@ extends Node
 var object = null
 
 
+func _ready():
+	#get_window().size = Vector2(320, 240)
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 func _process(delta):
 	pass
 
--- bin/Windows/start.sh
+++ bin/Windows/start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 
-prefix=game
+prefix="/home/igorek/Games/mygames/Escape from nothing"
 
 export WINEARCH=win64
 export script="$(readlink -f "${BASH_SOURCE[0]}")"
 export scriptdir="$(dirname "$script")"
 export LANG=ru_RU.UTF-8
 
-export WINEPREFIX="${scriptdir}"/$prefix
+export WINEPREFIX=$prefix
 wine game.exe
+#winecfg
