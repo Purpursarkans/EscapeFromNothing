a6d4b41c98c824d1a131466cdcb8235a3bc19760
Purpursarkans <goga.danilov10@gmail.com>
2024-02-04 03:15:44 +0400

    vsync

--- Script/G.gd
+++ Script/G.gd
@@ -4,11 +4,18 @@ var object = null
 
 var Sensivity : float = 10
 var FixSensivity : float = 0.0001
+var vsync : bool = false
+
 
 func _ready():
 	#get_window().size = Vector2(320, 240)
 	#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
 	pass
 
+func ChangeVsync():
+	if vsync:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_ENABLED)
+	else:
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 
 
